a9deed13678b59b0c12f7656eba7ec62816259e4
april <april@aprilharris.co>
2023-03-05 14:57:46 -0500

    minor edits

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var dataText = [ "coming soon."];
+  var dataText = ["coming soon."];
   
   // type one text in the typwriter
   // keeps calling itself until the text is finished
